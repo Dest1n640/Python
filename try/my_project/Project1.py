70a6529659c7716ab90a913c693adc808f4ad3f0
Dest1n <realmadridfunca@gmail.com>
2025-01-20 08:43:27 +0800

    Python

--- try/my_project/Project1.py
+++ try/my_project/Project1.py
@@ -1,7 +1,9 @@
 import random #Угадывание числа
 
 number = random.randint(1, 100)
-print(number)
+help = input("Хотите подсказку(да, нет): ")
+if help == 'да':
+    print(number)
 a = int(input("Отгадайте число от 1 до 100: "))
 while number != a:
     a = int(input("Отгадайте число от 1 до 100: "))
